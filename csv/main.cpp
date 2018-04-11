8d92b8ecddc8047aa918216fc43336b2bb04b6ce
itchencheng <itchencheng@gmail.com>
2018-04-11 23:42:09 +0800

    c++

---
+++ csv/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+
+int helpInfo()
+{
+    std::cout<< "this program is used for CSV reading" << std::endl;
+    std::cout<< "used STL string, cstring "<<std::endl;
+}
+
+
+int main()
+{
+    helpInfo();
+    return 0;
+}
